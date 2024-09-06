cd613170a674f7f6579f523a73a55f08430f9c46
Shivam-2309 <116469216+Shivam-2309@users.noreply.github.com>
2024-09-06 22:47:31 +0530

    Change

---
+++ Codeforces/TLEques/2Pointers/IraAndFlamenco.cpp
@@ -0,0 +1,30 @@
+// Author : Shivam Kapoor
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#define fast_io ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define debug(x) cerr << #x << " = " << x << endl
+#define print(v) cerr << #v << " = [ "; for (auto &el : v) cerr << el << " "; cerr << "]\n";
+#define print2d(v) cerr << #v << ":\n"; for (auto &row : v) { cerr << "[ "; for (auto &el : row) cerr << el << " "; cerr << "]\n"; }
+#define printmap(m) cerr << #m << ":\n"; for (auto &[key, value] : m) cerr << key << " -> " << value << "\n";
+#define printpp(v) cerr << #v << " = [ "; for (auto &[first, second] : v) cerr << "(" << first << ", " << second << ") "; cerr << "]\n";
+
+#define vll vector<ll>
+#define rev(v) reverse(all(v))
+#define srt(v) sort(all(v))
+#define all(v) (v).begin(), (v).end()
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+
+const ll mod7 = 1e9 + 7;
+
+void solve(){
+}
+
+int main(){
+    fast_io
+    ll t = 1;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
